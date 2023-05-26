c00f13cb93d420bd19b8e95d8534040ed607f31d
Allado, Abigail <110992588+abiww@users.noreply.github.com>
2023-05-27 00:51:55 +0800

    Create Panel.java

---
+++ Long Exam 1/Panel.java
@@ -0,0 +1,63 @@
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Image;
+
+import javax.swing.*;
+
+public class Panel{
+
+	JPanel panel;
+	
+	Panel() {
+		panel = new JPanel();
+		
+		panel.setBackground(Color.ORANGE);
+		panel.setBounds(0, 0, 770, 600);
+		panel.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 0));
+		
+	}
+	
+	public JPanel getPanel() {
+		return panel;
+	}
+
+	public void setPanel(JPanel panel) {
+		this.panel = panel;
+	}
+	
+	public void updateBounds(int x, int y, int width, int height) {
+		panel.setBounds(x, y, width, height);
+	}
+	
+	public void changeColorToWhite() {
+		panel.setBackground(Color.WHITE);
+	}
+	
+	public void updateLayout(int hgap, int vgap) {
+		panel.setLayout(new FlowLayout(FlowLayout.CENTER, hgap, vgap));
+	}
+	
+	public void addPicture(String imagePath, int width, int height) {
+		ImageIcon picture = new ImageIcon(imagePath);
+		Image image = picture.getImage();
+		Image resizedImage = image.getScaledInstance(width, height, java.awt.Image.SCALE_SMOOTH);
+		picture = new ImageIcon(resizedImage);
+		 
+		JLabel label = new JLabel();
+		label.setIcon(picture);
+		
+		panel.add(label);
+	}
+	
+	public void addButtons(JButton button) {
+		panel.add(button);
+	}
+	
+	public void setVisibilityToTrue() {
+			getPanel().setVisible(true); 
+	}
+	
+	public void setVisibilityToFalse() {
+		getPanel().setVisible(false);
+	}
+}
