3c49af8b85eadc2d94275e29cd7bfddfbddc4daa
Allado, Abigail <110992588+abiww@users.noreply.github.com>
2023-06-09 10:16:35 +0800

    Create HomePanel.java

---
+++ NU BE Queueing System/HomePanel.java
@@ -0,0 +1,20 @@
+
+class HomePanel extends ContentPanel {
+
+	HomePanel(int color) {
+		super(color);
+		
+		addImage("C:\\Users\\abiga\\OneDrive\\Desktop\\Project(NU BE Queueing Management System)\\HOME.png", 900, 500);
+	}
+
+}
+
+class AboutPanel extends ContentPanel{
+
+	AboutPanel(int color) {
+		super(color);
+		
+		addImage("C:\\Users\\abiga\\OneDrive\\Desktop\\Project(NU BE Queueing Management System)\\ABOUT.png", 900, 500);
+		
+	}
+}
