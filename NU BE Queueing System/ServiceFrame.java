3b9082a7276101eca6b7733e644d73f2129f4812
Allado, Abigail <110992588+abiww@users.noreply.github.com>
2023-06-13 14:10:52 +0800

    Update ServiceFrame.java

--- NU BE Queueing System/ServiceFrame.java
+++ NU BE Queueing System/ServiceFrame.java
@@ -5,6 +5,7 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.Image;
+import java.awt.MenuBar;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -13,8 +14,15 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JLayeredPane;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
+
+import nube.model.UserManagement;
+import nube.model.Users;
 
 public class ServiceFrame extends JFrame implements ActionListener{
 	private JPanel topPanel, topLeftPanel, topRightPanel;
@@ -22,13 +30,17 @@ public class ServiceFrame extends JFrame implements ActionListener{
 	private Image image, resizedImage;
 	private JLabel logoLabel;
 	private JButton orderSlip, reservation;
-        private JLayeredPane layeredPane;
+//	private JButton service;
+    private JLayeredPane layeredPane;
         
-        private OrderSlipPanel orderslipPanel;
-        private ReservationPanel reservationPanel;
+    private OrderSlipPanel orderslipPanel;
+    private ReservationPanel reservationPanel;
+    private JMenu menu;
+    private JMenuBar menuBar;
+    private JMenuItem logout;
 	
 
-	ServiceFrame(String title) {
+	ServiceFrame(String title, String firstName) {
 		setTitle(title);
 		
 		topPanel = new JPanel();
@@ -58,17 +70,57 @@ public class ServiceFrame extends JFrame implements ActionListener{
 		topRightPanel.setBackground(new Color(0x293478));
 		topRightPanel.setPreferredSize(new Dimension(1000, 80));
 		topRightPanel.setLayout(new FlowLayout(FlowLayout.TRAILING, 10, 0));
-	
-//		service = new JButton("SERVICE");
-//		service.setHorizontalAlignment(SwingConstants.CENTER);
-//		service.setFont(new Font("Tahoma", Font.BOLD, 18));
-//		service.setBackground(new Color(0x293478));
-//		service.setForeground(Color.WHITE);
-//		service.setPreferredSize(new Dimension(120,80));
-//		service.setBorderPainted(false);
-//		service.setFocusable(false);
-//		service.addActionListener(this);
 		
+		orderSlip = new JButton("ORDER SLIP");
+		orderSlip.setHorizontalAlignment(SwingConstants.CENTER);
+		orderSlip.setFont(new Font("Tahoma", Font.BOLD, 18));
+		orderSlip.setBackground(new Color(0x293478));
+		orderSlip.setForeground(Color.WHITE);
+		orderSlip.setPreferredSize(new Dimension(150,80));
+		orderSlip.setBorderPainted(false);
+		orderSlip.setFocusable(false);
+		orderSlip.addActionListener(this);
+		
+		menu = new JMenu("Howdy, " + firstName);
+		menu.setPreferredSize(new Dimension(150,80));
+		menu.setHorizontalTextPosition(SwingConstants.CENTER);
+		menu.setFont(new Font("Tahoma", Font.BOLD, 18));
+		menu.setForeground(Color.WHITE);
+		menu.setFocusable(false);
+		menu.setBorderPainted(false);
+		menu.addActionListener(this);
+		
+		menuBar = new JMenuBar();
+		menuBar.setBackground(new Color(0x293478));
+		
+		
+		logout = new JMenuItem("LOGOUT");
+		logout.setOpaque(true);
+		logout.setBackground(new Color(0XFF3333));
+		logout.setFont(new Font("Tahoma", Font.BOLD, 18));
+		logout.setPreferredSize(new Dimension(150, 20));
+		logout.setForeground(Color.WHITE);
+		logout.setFocusable(false);
+		logout.setBorderPainted(false);
+		logout.addActionListener(new ActionListener() {
+	        @Override
+	        public void actionPerformed(ActionEvent e) {
+	            if (e.getSource() == logout) {
+	            	int leave = JOptionPane.showConfirmDialog(null, "Are you sure you want to logout?", "Logout", JOptionPane.YES_NO_OPTION);
+					if (leave == JOptionPane.YES_OPTION) {
+						JOptionPane.showMessageDialog(null, "Thank you for using NU Bulldog Exchange Queueing Management!");
+						
+						dispose();
+						
+						HomeFrame home = new HomeFrame("");
+					}
+	            }
+	        }
+	    });
+
+		menu.add(logout);
+		menuBar.add(menu);
+
 		orderSlip = new JButton("ORDER SLIP");
 		orderSlip.setHorizontalAlignment(SwingConstants.CENTER);
 		orderSlip.setFont(new Font("Tahoma", Font.BOLD, 18));
@@ -89,25 +141,25 @@ public class ServiceFrame extends JFrame implements ActionListener{
 		reservation.setFocusable(false);
 		reservation.addActionListener(this);
 		
-//		topRightPanel.add(service);
 		topRightPanel.add(orderSlip);
 		topRightPanel.add(reservation);
+		topRightPanel.add(menuBar);
 		
 		topPanel.add(topLeftPanel, BorderLayout.WEST);
 		topPanel.add(topRightPanel, BorderLayout.EAST);
                 
-                layeredPane = new JLayeredPane();
+        layeredPane = new JLayeredPane();
 		layeredPane.setBounds(0, 80, 1185, 585);
                 
-                orderslipPanel = new OrderSlipPanel(0x29FF78);
+        orderslipPanel = new OrderSlipPanel(0x29FF78);
                 
-                reservationPanel = new ReservationPanel(0xFFFF78);
+        reservationPanel = new ReservationPanel(0xFFFF78);
                 
-                layeredPane.add(orderslipPanel.getPanel(), Integer.valueOf(1));
+        layeredPane.add(orderslipPanel.getPanel(), Integer.valueOf(1));
 		layeredPane.add(reservationPanel.getPanel(), Integer.valueOf(0));
 		
 		add(topPanel);
-                add(layeredPane);
+        add(layeredPane);
 		
 		setSize(new Dimension(1200, 700));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -120,23 +172,26 @@ public class ServiceFrame extends JFrame implements ActionListener{
 	public void actionPerformed(ActionEvent e) {
 		
 		if (e.getSource().equals(orderSlip)) {
-//			service.setForeground(Color.white);
 			orderSlip.setForeground(Color.orange);
 			reservation.setForeground(Color.white);
+			menu.setForeground(Color.white);
                         
-                        orderslipPanel.setVisible(true);
-                        reservationPanel.setVisible(false);
+            orderslipPanel.setVisible(true);
+            reservationPanel.setVisible(false);
 		
 		} else if (e.getSource().equals(reservation)) {
-//			service.setForeground(Color.white);
 			orderSlip.setForeground(Color.white);
 			reservation.setForeground(Color.orange);
+			menu.setForeground(Color.white);
                         
-                        orderslipPanel.setVisible(false);
-                        reservationPanel.setVisible(true);
+            orderslipPanel.setVisible(false);
+            reservationPanel.setVisible(true);
 		
+		} else if (e.getSource().equals(menu)) {
+			orderSlip.setForeground(Color.white);
+			reservation.setForeground(Color.white);
+			menu.setForeground(Color.orange);
 		}
 		
 	}
-
 }
