8d21a3b76b710e1fab965b7b10e1db68144776e5
Allado, Abigail <110992588+abiww@users.noreply.github.com>
2023-06-21 14:42:36 +0800

    Add files via upload

---
+++ Long Exam 3/src/integration/Main.java
@@ -0,0 +1,10 @@
+package integration;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		PortfolioIntegration wonderPets = new PortfolioIntegration();
+		wonderPets.portfolioIntegrationRun();
+	}
+
+}
