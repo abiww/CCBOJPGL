010689c9cebb79761a13cf1c270d23fc02c53395
Allado, Abigail <110992588+abiww@users.noreply.github.com>
2023-06-21 14:29:36 +0800

    Create Main.java

---
+++ Long Exam 3/src/integration/Main.java
@@ -0,0 +1,10 @@
+//This class serves as the runner
+
+package integration;
+
+public class Main {
+	public static void main(String[] args) {
+		PortfolioIntegration wonderPets = new PortfolioIntegration();
+		wonderPets.portfolioIntegrationRun();
+	}
+}
